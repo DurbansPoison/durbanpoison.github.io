2fd0c7434f045e7dab2a277be812658f239e8fef
Ryan O’Boyle <durbanpoison@protonmail.com>
2022-08-27 19:05:19 -0700

    got the model renderer to work

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Header from './components/header';
 import Links from './components/links/index'
-
+import StlContainer from './components/stlContainer/index';
 function App() {
   return (
     <div className="App">
     <Header />
 		<Links />
+		<StlContainer />
     </div>
   );
 }
---
+++ src/components/stlContainer/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import StlShower from '../stlShower/index';
+
+function StlContainer() {
+  return (
+    <div>
+      <div className='stlCont'>
+				<StlShower />
+			</div>
+    </div>
+  );
+}
+
+export default StlContainer;
