15aead50266f0aebfd0d151ca93f72bb0f4bff1c
Ryan O’Boyle <durbanpoison@protonmail.com>
2022-09-05 10:23:40 -0700

    added anylitcs

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
+import React from 'react';
 import './App.css';
 import Header from './components/header';
 import Links from './components/links/index'
-import StlContainer from './components/stlContainer/index';
+
 function App() {
   return (
     <div className="App">
     <Header />
 		<Links />
-		<StlContainer />
+
     </div>
   );
 }
